99b3de1b8454b6dbe78032e26c9cb7f77106a526
Guilherme <127777746+GuilhermeCarrer@users.noreply.github.com>
2026-02-02 21:42:22 -0300

    Add files via upload

---
+++ Teste/Paises.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Teste
+{
+    partial class Paises
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.crudpaisesDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.crudpaisesDataSet = new Teste.crudpaisesDataSet();
+            this.crudpaisesDataSet1 = new Teste.crudpaisesDataSet1();
+            this.paisesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.paisesTableAdapter = new Teste.crudpaisesDataSet1TableAdapters.paisesTableAdapter();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.crudpaisesDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crudpaisesDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crudpaisesDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paisesBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(63, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CADASTRAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(165, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "EDITAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(265, 204);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "APAGAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(479, 93);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(254, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "PESQUISAR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Código País";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "População";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(208, 93);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Área total";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(63, 109);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(112, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(211, 47);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(149, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(211, 109);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(149, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // crudpaisesDataSetBindingSource
+            // 
+            this.crudpaisesDataSetBindingSource.DataSource = this.crudpaisesDataSet;
+            this.crudpaisesDataSetBindingSource.Position = 0;
+            // 
+            // crudpaisesDataSet
+            // 
+            this.crudpaisesDataSet.DataSetName = "crudpaisesDataSet";
+            this.crudpaisesDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // crudpaisesDataSet1
+            // 
+            this.crudpaisesDataSet1.DataSetName = "crudpaisesDataSet1";
+            this.crudpaisesDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // paisesBindingSource
+            // 
+            this.paisesBindingSource.DataMember = "paises";
+            this.paisesBindingSource.DataSource = this.crudpaisesDataSet1;
+            // 
+            // paisesTableAdapter
+            // 
+            this.paisesTableAdapter.ClearBeforeFill = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(476, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Pesquisar (Codigo do País)";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(479, 47);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(254, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // Paises
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Paises";
+            this.Text = "CRUD Países";
+            this.Load += new System.EventHandler(this.Paises_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.crudpaisesDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crudpaisesDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crudpaisesDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paisesBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.BindingSource crudpaisesDataSetBindingSource;
+        private crudpaisesDataSet crudpaisesDataSet;
+        private crudpaisesDataSet1 crudpaisesDataSet1;
+        private System.Windows.Forms.BindingSource paisesBindingSource;
+        private crudpaisesDataSet1TableAdapters.paisesTableAdapter paisesTableAdapter;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ Teste/Paises.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Teste
+{
+    public partial class Paises : Form
+    {
+        public Paises()
+        {
+            InitializeComponent();
+        }
+        SqlConnection sqlCon = null;
+        string strCon = ConfigurationManager.ConnectionStrings["MinhaConexao"].ConnectionString;
+        private string strSql = string.Empty;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            strSql = "insert into paises (cod_pais, nome, populacao, area_total) values(@cod_pais, @nome, @populacao, @area_total)";
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+            comando.Parameters.Add(@"cod_pais", SqlDbType.Int).Value = textBox1.Text;
+            comando.Parameters.Add(@"nome", SqlDbType.VarChar).Value = textBox3.Text;
+            comando.Parameters.Add(@"populacao", SqlDbType.Int).Value = textBox2.Text;
+            comando.Parameters.Add(@"area_total", SqlDbType.Decimal).Value = textBox4.Text;
+
+            try
+            {
+                sqlCon.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Cadastro realizado com sucesso!");
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            strSql = "select * from paises where cod_pais=@cod_pais";
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+            comando.Parameters.Add("@cod_pais", SqlDbType.Int).Value = textBox5.Text;
+
+
+            try
+            {
+                if (textBox5.Text == string.Empty)
+                {
+                    throw new Exception("Você precisa digitar algo");
+                }
+                sqlCon.Open();
+
+
+                SqlDataReader dr = comando.ExecuteReader();
+
+                if (dr.Read()) 
+                {
+                    textBox1.Text = Convert.ToString(dr["cod_pais"]);
+                    textBox2.Text = Convert.ToString(dr["populacao"]);
+                    textBox3.Text = Convert.ToString(dr["nome"]);
+                    textBox4.Text = Convert.ToString(dr["area_total"]);
+
+                }
+                else
+                {
+                    throw new Exception("Não encontrado");
+                }
+
+              
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close(); 
+            }
+            
+                
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente excluir este país?", "CUIDADO", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.No) 
+            {
+                MessageBox.Show("Operação cancelada!");
+            }
+            else
+
+
+            {
+                strSql = "delete from paises where cod_pais =@cod_pais";
+                sqlCon = new SqlConnection(strCon);
+                SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+                comando.Parameters.Add("@cod_pais", SqlDbType.Int).Value = textBox1.Text;
+
+                try
+                {
+                    sqlCon.Open();
+                    comando.ExecuteNonQuery();
+                    MessageBox.Show("País excluído com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    sqlCon.Close();
+                }
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+            }
+                
+        }
+
+        private void Paises_Load(object sender, EventArgs e)
+        {
+           
+            // TODO: esta linha de código carrega dados na tabela 'crudpaisesDataSet1.paises'. Você pode movê-la ou removê-la conforme necessário.
+            this.paisesTableAdapter.Fill(this.crudpaisesDataSet1.paises);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            strSql = "update paises set cod_pais =@cod_pais, nome=@nome, populacao=@populacao, area_total=@area_total where cod_pais = @id_pais";
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+
+
+
+            comando.Parameters.Add("@id_pais",SqlDbType.Int).Value = textBox1.Text;  
+
+            comando.Parameters.Add(@"cod_pais", SqlDbType.Int).Value = textBox1.Text;
+            comando.Parameters.Add(@"nome", SqlDbType.VarChar).Value = textBox3.Text;
+            comando.Parameters.Add(@"populacao", SqlDbType.Int).Value = textBox2.Text;
+            comando.Parameters.Add(@"area_total", SqlDbType.Decimal).Value = textBox4.Text;
+
+
+            try
+            {
+                sqlCon.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Cadastro atualizado com sucesso!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close(); 
+            }
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+    }
+}
